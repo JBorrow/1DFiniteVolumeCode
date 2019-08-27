6540db39463f20cba3c485467a7d697d1db5d285
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-08-27 16:57:06 +0100

    Updated plotting script to use more useful marker sizes

--- plot_output.py
+++ plot_output.py
@@ -15,17 +15,17 @@ positions = np.linspace(0, 1, len(data[0]))
 fig, ax = plt.subplots(1, 3, figsize=(8, 3), sharex=True)
 ax = ax.flatten()
 
-ax[0].scatter(positions, data[4])
+ax[0].scatter(positions, data[4], s=1.0)
 ax[0].set_ylabel("Density")
 
-ax[1].scatter(positions, data[5])
+ax[1].scatter(positions, data[5], s=1.0)
 ax[1].set_ylabel("Velocity")
 ax[1].set_xlabel("Position")
 
-ax[2].scatter(positions, data[6])
+ax[2].scatter(positions, data[6], s=1.0)
 ax[2].set_ylabel("Pressure")
 
-ax[2].set_xlim(0, 1)
+ax[2].set_xlim(0.25, 0.75)
 
 fig.tight_layout()
 fig.savefig("output.pdf")
